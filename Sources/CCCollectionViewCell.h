24138e58d5e8c03a8b4ff8226f18a88ba0a47242
Igor Bogatchuk <bogatchuk.i@gmail.com>
2014-03-12 18:02:07 +0200

    Added collectionViewCell

---
+++ Sources/CCCollectionViewCell.h
@@ -0,0 +1,13 @@
+//
+//  CCCollectionViewCell.h
+//  CyclingCollectionView
+//
+//  Created by Igor Bogatchuk on 3/12/14.
+//  Copyright (c) 2014 Igor Bogatchuk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCCollectionViewCell : UICollectionViewCell
+
+@end
---
+++ Sources/CCCollectionViewCell.m
@@ -0,0 +1,31 @@
+//
+//  CCCollectionViewCell.m
+//  CyclingCollectionView
+//
+//  Created by Igor Bogatchuk on 3/12/14.
+//  Copyright (c) 2014 Igor Bogatchuk. All rights reserved.
+//
+
+#import "CCCollectionViewCell.h"
+
+@implementation CCCollectionViewCell
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
