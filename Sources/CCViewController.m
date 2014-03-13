1b679bc7ca5a95ecb0e48dec278efa0898ee4dd2
Igor Bogatchuk <bogatchuk.i@gmail.com>
2014-03-13 13:44:49 +0200

    implemented continuous scroll with duobling of datasource items

--- Sources/CCViewController.m
+++ Sources/CCViewController.m
@@ -11,9 +11,13 @@
 
 NSString* const kCCCollectionCellIdentifier = @"CollectionCell";
 
-@interface CCViewController ()<UICollectionViewDataSource, UICollectionViewDelegate>
+@interface CCViewController ()<UICollectionViewDataSource, UICollectionViewDelegate, UIScrollViewDelegate>
 
 @property (weak, nonatomic) IBOutlet UICollectionView *collectionView;
+@property (nonatomic, strong) NSArray* dataSource;
+
+@property CGFloat topThreshold;
+@property CGFloat bottomThreshold;
 
 @end
 
@@ -23,13 +27,15 @@ NSString* const kCCCollectionCellIdentifier = @"CollectionCell";
 {
     [super viewDidLoad];
     
+    self.dataSource = @[@"Sunday", @"Monday", @"Tuesday", @"Wednesday", @"Thursday", @"Friday", @"Saturday"];
+    
     UICollectionViewFlowLayout* layout = [UICollectionViewFlowLayout new];
-    layout.itemSize = CGSizeMake(145, 192);
-	layout.sectionInset = UIEdgeInsetsMake(0, 0, 0, 0);
+    layout.itemSize = CGSizeMake(140, 190);
+	layout.sectionInset = UIEdgeInsetsMake(10, 10, 10, 10);
 	layout.scrollDirection = UICollectionViewScrollDirectionHorizontal;
 	[self.collectionView setCollectionViewLayout:layout animated:YES];
     
-
+    self.collectionView.showsHorizontalScrollIndicator = NO;
 }
 
 - (void)didReceiveMemoryWarning
@@ -47,14 +53,47 @@ NSString* const kCCCollectionCellIdentifier = @"CollectionCell";
 
 - (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section
 {
-    return 5;
+    return [self.dataSource count] * 2;
 }
 
 - (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath
 {
     CCCollectionViewCell *cell = [self.collectionView dequeueReusableCellWithReuseIdentifier:kCCCollectionCellIdentifier forIndexPath:indexPath];
-    [cell.label setText:[NSString stringWithFormat:@"%d",indexPath.row]];
+    
+    NSInteger indexOfItemInDataSource = indexPath.row;
+    
+    if (indexOfItemInDataSource > [self.dataSource count] - 1)
+    {
+        indexOfItemInDataSource -= [self.dataSource count];
+    }
+    
+    [cell.label setText:self.dataSource[indexOfItemInDataSource]];
+    if (indexOfItemInDataSource == 0 || indexOfItemInDataSource == 6)
+    {
+        cell.backgroundColor = [UIColor greenColor];
+    }
+    else
+    {
+        cell.backgroundColor = [UIColor blueColor];
+    }
+    
     return cell;
+}
+
+#pragma mark - ScrollViewDelegate
+
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView
+{
+    NSInteger itemsCount = self.dataSource.count * 2;
+    
+    if (scrollView.contentOffset.x < (scrollView.contentSize.width / itemsCount))
+    {
+        scrollView.contentOffset = CGPointMake(scrollView.contentOffset.x + (scrollView.contentSize.width / 2), scrollView.contentOffset.y);
+    }
+    if (scrollView.contentOffset.x > ((scrollView.contentSize.width * (itemsCount / 2 + 1))/ itemsCount))
+    {
+        scrollView.contentOffset = CGPointMake(scrollView.contentOffset.x - (scrollView.contentSize.width / 2), scrollView.contentOffset.y);
+    }
 }
 
 @end
